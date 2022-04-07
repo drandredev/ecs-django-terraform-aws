d9aad74ab98563b4384093c65a1562abdf09144f
drandredev <augusto.delgado@tecsup.edu.pe>
2022-04-07 13:06:11 -0500

    django middleware - script to update ecs

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'hello_django.middleware.HealthCheckMiddleware',  # new
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
